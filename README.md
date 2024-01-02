06d53e23a8f7b8fb4692aecde1a61df85ab44e31
DrewBornt <andrew.bornt@gmail.com>
2024-01-02 16:43:02 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# A Simple SDDM Theme Switcher
+
+#### md5sum:
+4f9fa2c23169c70c2211c8d8564a9adc
+
+### How to use: 
+./simplethemeswitcher <"theme-name">
+./simplethemeswitcher list (to be implemented)
+
+
+## Compiling
+
+If you'd like to just compile the code yourself, ensure gcc is installed on your system.
+Then navigate into the directory with 'main.c'
+```
+gcc main.c -o simplethemeswitcher
+```
+
+#### Written by: Drew Bornt 
+
+After installing Arch on my personal laptop and trying to navigate myself around how to use it, I wanted to be able to easily change my SDDM theme from the terminal without opening a file or other program.
+
+Sure, if you're using a more fleshed out distro, you'll already have some GUI tool to set a theme.
+
+But if I know the name of the theme, why can't I just change it from a terminal without having to go and edit a cofig file? That's exactly what this program will do for you.
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
-    A Simple GTK Theme Switcher
+    A Simple SDDM Theme Switcher
     
     Written By: Drew Bornt
 */
@@ -8,36 +8,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-int fileExists();
-
 int main(int argc, char* argv[])
 {
-    const char *homedir = getenv("HOME");
 
-    char filePath[1024];
-
-    snprintf(filePath, sizeof(filePath), "%s/.config/gtk-3.0/settings.ini", homedir);
-
-    printf("%s\n", filePath);
-
-    if (fileExists(filePath) == 1)
+    // Checks the args count
+    if (argc != 2)
     {
-        printf("File opened successfully\n");
-    }
-
-    return 1;
-
-}
-
-int fileExists(char* filePath)
-{
-    FILE *file;
-
-    if ((file = fopen(filePath, "r")))
-    {
-        fclose(file);
+        printf("Usage: ./simplethemeswitcher <\"theme name\">\n");
         return 1;
     }
 
-    return 0;
+    // Gets the current logged-in user's home directory
+    FILE *originalSettingsFP, *newSettingsFP;
+    char *originalSettingsFileName = "/etc/sddm.conf.d/default.conf";
+    char *tempSettingsFileName = "/etc/sddm.conf.d/temp.txt";
+
+    originalSettingsFP = fopen(originalSettingsFileName, "r");
+    if (!originalSettingsFP)
+    {
+        printf("Failed to open settings file.\n Does this have read permissions?\n");
+        return 1; //unsuccessful execution
+    }
+
+    newSettingsFP = fopen(tempSettingsFileName, "w");
+    if (!newSettingsFP)
+    {
+        printf("Error writing to a new config file.\n Does this have write permissions?\n");
+        return 1; //unsuccessful execution
+    }
+
+    // Takes the theme name from the commandline input
+    char themeName[65];
+    strncpy(themeName, argv[1], sizeof(themeName) - 1);
+    themeName[sizeof(themeName) - 1] = '\0';
+
+    // Set up a buffer for fgets
+    char lineBuffer[256];
+
+    // Sets the line we need to append in the theme file
+    char lineToAdd[128];
+    snprintf(lineToAdd, sizeof(lineToAdd), "Current=%s", themeName);
+
+    // Loop through each line of the original file
+    while (fgets(lineBuffer, sizeof(lineBuffer), originalSettingsFP) != NULL)
+    {
+        // If the line starts with this, we write to the new settings file the line with the new theme
+        if (strncmp(lineBuffer, "Current=", 7) == 0)
+        {
+            fprintf(newSettingsFP, lineToAdd);
+        }
+        // If the line doesn't match that, we don't want to change the line
+        else
+        {
+            fputs(lineBuffer, newSettingsFP);
+        }
+    }
+    
+    fclose(newSettingsFP);
+    fclose(originalSettingsFP);
+    
+    char originalSettingsFileNameBackup[1024];
+    snprintf(originalSettingsFileNameBackup, sizeof(originalSettingsFileName), "%s.bak", originalSettingsFileName);
+
+    // Delete the original file
+    rename(originalSettingsFileName, originalSettingsFileNameBackup);
+    // Rename the new file to match the old
+    rename(tempSettingsFileName, originalSettingsFileName);
+    
+    return 0; // successful execution
+
 }
